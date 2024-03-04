ea0046b1f9e186598c73ce2a1e377e7f26ce5731
jonathan.lp <jlopezpe@unal.edu.co>
2024-03-03 21:47:11 -0500

    configurating properties methods

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,61 @@
+package tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.MalformedInputException;
+import java.util.Properties;
+
+import static io.appium.java_client.internal.CapabilityHelpers.getCapability;
+import static sun.awt.FontConfiguration.loadProperties;
+
+public class BaseTest {
+
+    private static final String PROPERTIES_FILE="src/test/resources/config.properties";
+    private static final Properties properties= new Properties();
+
+    public static AndroidDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void environmentSetUp(){
+        loadProperties();
+        UiAutomator2Options capabilities= new UiAutomator2Options();
+        setUpCapabilities(capabilities);
+
+        try{
+            driver=new AndroidDriver(new URL("http://127.0.0.1:4723"),capabilities);
+        }
+        catch (MalformedURLException exception){
+            System.out.println(exception.getMessage());
+        }
+
+    }
+
+    public void loadProperties(){
+        try{
+            FileInputStream fileInputStream=new FileInputStream(PROPERTIES_FILE);
+            properties.load(fileInputStream);
+        }
+        catch (IOException e){
+            throw new RuntimeException("Error loading properties"+PROPERTIES_FILE);
+        }
+    }
+
+    public static void setUpCapabilities(UiAutomator2Options capabilities){
+
+        capabilities.setPlatformName(getCapability("platformName"));
+        capabilities.setDeviceName(getCapability("deviceName"));
+        capabilities.setAppPackage(getCapability("appPackage"));
+        capabilities.setAppActivity(getCapability("appActivity"));
+    }
+
+    public static String getCapability(String variable){
+        return properties.getProperty(variable);
+    }
+
+}
